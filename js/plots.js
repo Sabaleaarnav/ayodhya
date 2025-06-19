db456cbb91066f3eb691a95a00146c4a89908dc5
Sabaleaarnav <sabaleaarnav@gmail.com>
2025-06-18 22:21:23 -0400

    Update plots.js

--- js/plots.js
+++ js/plots.js
@@ -1,6 +1,6 @@
 (async ()=>{
   // choose the phase or merge all jsons – here we merge them
-  const files = ["Finance.json","Plot Details.json"];
+  const files = ["Plots.json"];
   const statusMap = Object.assign(
       {}, ...(await Promise.all(files.map(f=>fetch(`data/${f}`).then(r=>r.json()))))
   );
